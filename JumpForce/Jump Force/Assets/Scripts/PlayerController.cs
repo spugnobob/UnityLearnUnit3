321eb40749d6f7a5976147aeab4593ed2d9ffc84
Jonathan <luquinj1@roverkids.org>
2026-04-30 13:50:39 -0400

    3.4

--- JumpForce/Jump Force/Assets/Scripts/PlayerController.cs
+++ JumpForce/Jump Force/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,16 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip CrashSound;
+    private AudioSource playerAudio;
+
 
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
@@ -19,7 +26,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) 
+        {
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            dirtParticle.Stop();
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
@@ -30,10 +40,14 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            dirtParticle.Play();
             isOnGround = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAudio.PlayOneShot(CrashSound, 1.0f);
+            dirtParticle.Stop();
+            explosionParticle.Play();
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
